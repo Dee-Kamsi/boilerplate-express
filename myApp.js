ad6cd03584e345c5d210f6c096d1f43efc64cf63
Dee-Kamsi <dkobinwa@mail.lipscomb.edu>
2023-10-30 20:23:56 -0500

    served an html file

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ app.get("/", function (req, res) {
 })
 
 app.get("/", (req, res)=> {
-    res.sendFile(__direname + '/views/index.html');
+    res.sendFile("html"__direname + '/views/index.html');
 })
 
 
