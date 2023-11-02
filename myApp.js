36d680caeff9dfc34db91fb25dbc569858cef5ab
Dee-Kamsi <dkobinwa@mail.lipscomb.edu>
2023-11-02 13:39:57 -0500

    *edited message*

--- myApp.js
+++ myApp.js
@@ -1,59 +1,17 @@
 let express = require('express'); 
 let app = express(); 
 
-let bodyParser = require("body-parser"); 
+//console.log("Hello World")
 
-app.use("/public",express.static(__dirname + "/public")) 
-app.use(bodyParser.urlencoded({extended: false})); 
-app.use((req, res, next) => { 
-console.log(`${req.method} ${req.path} - ${req.ip}`); next(); 
-}); 
-//1 
-//console.log("Hello World") 
-//2 
-// app.get("/",(req,res) => { 
-// res.send("Hello Express") 
-//}) 
-//3 
-app.get("/",(req,res) =>{ 
-res.sendFile(__dirname + "/views/index.html"); }) 
-let message ={message:"Hello json"}; 
-app.get("/json", (req, res) => { 
-if (process.env["MESSAGE_STYLE"] === "uppercase") { res.json({"message":"HELLO JSON"}); 
-} else { 
-res.json({"message":"Hello json"}); 
-} 
-}); 
-app.get("/now", (req, res, next) => { 
-req.time = new Date().toString(); 
-next();
-}, (req, res) => { 
-res.json({"time": req.time}); 
-}); 
-app.get("/:word/echo", (req, res) => { 
-res.json({"echo": req.params.word}); 
-}); 
-app.get("/name", (req, res) => { 
-res.json({name: req.query.first + " " + req.query.last}); }); 
-app.post("/name", (req, res) => { 
-res.json({name: req.body.first + " " + req.body.last}); });
+// app.get("/", (req, res) => {
+// res.send("Hello Express")
+// })
 
+app.use("/public", express.static(__dirname + "/public")) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+})
 
 
 
